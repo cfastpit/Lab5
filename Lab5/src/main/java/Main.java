0bf42fd181409d1c97c94fa05f447df66f281725
theki <theki@DESKTOP-530HBLH>
2020-04-15 19:34:56 -0500

    1 is done

--- Lab5/src/main/java/Main.java
+++ Lab5/src/main/java/Main.java
@@ -1,13 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author theki
- */
+import java.util.ArrayList;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        int test = 5;
+        ArrayList<Integer> array1 = new ArrayList<Integer>();
+        array1.add(3);
+        array1.add(34);
+        array1.add(35);
+        array1.add(9);
+        array1.add(19);
+        ArrayList bigerThan = isItBiger(array1, test);
+        System.out.println(test + " is biggerthen" + bigerThan);
+
+    }
+
+    public static ArrayList isItBiger(ArrayList array1, int test) {
+        int testnumber = test;
+
+        ArrayList<Integer> array2 = new ArrayList<Integer>();
+        for (int n = 0; n < array1.size(); n++) {
+            if (test < (Integer) array1.get(n)) {
+                array2.add((Integer) array1.get(n));
+            }
+
+        }
+
+        return array2;
+    }
 }
